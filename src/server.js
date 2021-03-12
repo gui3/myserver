872ed6e5fad14f02993177827febaa1437330e49
gui3 <gui.silvent@gmail.com>
2021-03-12 22:43:26 +0100

    update disable_bot ability

--- src/server.js
+++ src/server.js
@@ -6,34 +6,40 @@ const getConfig = require("./utils/getConfig.js")
 
 const config = getConfig()
 
-if (!config.disabled) {
-  const { app } = require("./express/app.js")
-  const { discordClient } = require("./discord/client.js")
-  const errorHandler = require("./utils/errorHandler.js")
-  const Linker = require("./utils/Linker.js")
-  
-  // linking bot and server
-  const linker = new Linker()
-  linker.link("server", "express", app)
-  linker.link("taya", "discord", discordClient)
-  
-  // connect discord bot
-  discordClient.login(process.env.DISCORD_BOT_TOKEN)
-  .then(_ => {
-    // send boot message in discord
-    discordClient._do.sendToDevs(
-      "Hey ! bot booted at :\n" + new Date().toISOString()
+
+const { app } = require("./express/app.js")
+const { discordClient } = require("./discord/client.js")
+const errorHandler = require("./utils/errorHandler.js")
+const Linker = require("./utils/Linker.js")
+
+// linking bot and server
+const linker = new Linker()
+linker.link("server", "express", app)
+linker.link("taya", "discord", discordClient)
+
+// connect discord bot
+discordClient.login(config.disable_bot ? null : process.env.DISCORD_BOT_TOKEN)
+.then(_ => {
+  // send boot message in discord
+  discordClient._do.sendToDevs(
+    "Hey ! bot booted at :\n" + new Date().toISOString()
+  )
+})
+.catch(err => {
+  // if bot disabled for test purposes
+  if (err.code === "TOKEN_INVALID"
+  && config.disable_bot === true) 
+  {
+    console.error(
+      "!!!! the discord bot is DISABLED, see CONFIG.yaml to start it up"
     )
-  })
-  .catch(errorHandler)
+  }
   
-  // connect express app
-  const PORT = process.env.PORT || 5000
-  
-  app.listen(PORT, () => {
-    console.log("my little server listenning on " + PORT)
-  })
-}
-else {
-  console.log("this app is disabled, see CONFIG.yaml to start it up")
-}
+})
+
+// connect express app
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log("my little server listenning on " + PORT)
+})
